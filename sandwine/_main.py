0d896addf8df8946f41ccf58e31bd84909aa2a78
Sebastian Pipping <sebastian@pipping.org>
2023-03-27 02:58:09 +0200

    Pass --unshare-user to Bubblewrap
    .. because --unshare-all includes --unshare-user-try
    (with emphasis on "try") rather than strict --unshare-user.

--- sandwine/_main.py
+++ sandwine/_main.py
@@ -233,7 +233,7 @@ def create_bwrap_argv(config):
     ]
     env_tasks = {var: None for var in ['HOME', 'TERM', 'USER', 'WINEDEBUG']}
     env_tasks['container'] = 'sandwine'
-    unshare_args = ['--unshare-all']
+    unshare_args = ['--unshare-user', '--unshare-all']
 
     argv = ArgvBuilder()
 
