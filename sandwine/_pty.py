8677601de09a63c67eb3ebd2a57f40661c0918c5
Sebastian Pipping <sebastian@pipping.org>
2023-12-16 19:40:21 +0100

    _pty.py: Cheaply improve PTY experience

--- sandwine/_pty.py
+++ sandwine/_pty.py
@@ -15,15 +15,54 @@
 # You should have received a copy of the GNU General Public License along
 # with sandwine. If not, see <https://www.gnu.org/licenses/>.
 
+import fcntl
 import os
 import pty
+import signal
+import struct
+import termios
+from functools import partial
+from unittest.mock import patch
+
+import pexpect
+
+
+def _copy_window_size(to_p: pexpect.spawn, from_fd: int):
+    if not os.isatty(from_fd):
+        return
+
+    s = struct.pack('HHHH', 0, 0, 0, 0)
+    a = struct.unpack('HHHH', fcntl.ioctl(from_fd, termios.TIOCGWINSZ, s))
+    if not to_p.closed:
+        to_p.setwinsize(a[0], a[1])
+
+
+def _handle_sigwinch(p: pexpect.spawn, *_):
+    _copy_window_size(to_p=p, from_fd=pty.STDOUT_FILENO)
 
 
 def pty_spawn_argv(argv):
-    wait_status = pty.spawn(argv)
+    # NOTE: This implementation is known to not be the real deal,
+    #       e.g. Ctrl+Z will not work at all.  It's just a cheap win over
+    #       the previous implementation before the real deal.
+    p = pexpect.spawn(command=argv[0], args=argv[1:], timeout=None)
 
-    exit_code = os.waitstatus_to_exitcode(wait_status)
-    if exit_code < 0:  # e.g. -2 for "killed by SIGINT"
-        exit_code = 128 - exit_code  # e.g. -2 -> 130
+    signal.signal(signal.SIGWINCH, partial(_handle_sigwinch, p))
+
+    _copy_window_size(p, from_fd=pty.STDOUT_FILENO)
+
+    if os.isatty(pty.STDIN_FILENO):
+        p.interact()
+    else:
+        with patch('tty.tcgetattr'), patch('tty.tcsetattr'), patch('tty.setraw'):
+            p.interact()
+
+    p.wait()
+    p.close()
+
+    if p.signalstatus is not None:
+        exit_code = 128 + p.signalstatus
+    else:
+        exit_code = p.exitstatus
 
     return exit_code
