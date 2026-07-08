hartwork/sandwine

commit c3d8f45b2587abb1268bae95a271c5b6abb9a63e

wine *.lnk, wine start: prevent process is killed
Both `wine start` and `wine *.lnk launch a process and exit before
that process has terminated. Do not kill the wineserver, but wait
for the wineserver to exit spontaneously to be sure the launched
process has terminated. Otherwise, the process will be killed
before it has terminated.

This may also require option --no-pty: the script tool may exit with
exit code 3.

Signed-off-by: Bastiaan Stougie <wififreedom2026@protonmail.com>

fixes

sandwine/_main.py
@@ -613,10 +613,16 @@ def create_bwrap_argv(config):
         parts = [
             '"$0" "$@"',
             "ret=$?",
-            '"${wineserver}" -k ' + str(signal.SIGTERM),
-            '"${wineserver}" -w',
-            "exit ${ret}",
         ]
+        if (config.argv_0.casefold() != "start") and not config.argv_0.casefold().endswith(".lnk"):
+            # make the wineserver exit faster.
+            parts.append('"${wineserver}" -k ' + str(signal.SIGTERM))
+        parts.extend(
+            [
+                '"${wineserver}" -w',
+                "exit ${ret}",
+            ]
+        )
         argv.add("sh", "-c", " ; ".join(parts))
 
     # Add winecfg

commit d973038ef7307f360dc8b02e030001c94f1a0359

Fix crash for running plain `sandwine`
Regression from commit c3d8f45b2587abb1268bae95a271c5b6abb9a63e in sandwine 8.0.0

sandwine/_main.py
@@ -620,7 +620,9 @@ def create_bwrap_argv(config):
             '"$0" "$@"',
             "ret=$?",
         ]
-        if (config.argv_0.casefold() != "start") and not config.argv_0.casefold().endswith(".lnk"):
+        if config.argv_0 is None or (
+            config.argv_0.casefold() != "start" and not config.argv_0.casefold().endswith(".lnk")
+        ):
             # make the wineserver exit faster.
             parts.append('"${wineserver}" -k ' + str(signal.SIGTERM))
         parts.extend(